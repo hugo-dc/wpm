85ed5f933c6c74afcb7be30b575e0e68dee87b81
hugo-dc <hugo.delacruz@live.com.mx>
2013-05-31 12:00:26 -0600

    Installer, update

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BIN_DIR=`dirname ~/bin`
+
+if [ ! -d "$BIN_DIR" ]; then 
+    mkdir $BIN_DIR
+    echo "Adding $BIN_DIR to your PATH..." 
+    
+    source ~/.bashrc
+fi
+
+GIT=git 
+
+if [ -z $(which "$GIT") ] ; then 
+    echo "error: git is required to install wpm. wpm is designed to GitBash"
+    exit 1
+fi
+
+cd $BIN_DIR/bin
+echo "Cloning wpm repository..."
+git clone git://github.com/hugo-dc/wpm.git
+echo "Adding wpm to your PATH..."
+
+echo 
+echo -n $'  echo \'export PATH="PATH:';
+echo "$BIN_DIR/bin/wpm\"' >> ~/.bashrc";
+echo 'export PATH=$PATH:~/bin/wpm' >> ~/.bashrc
+echo "source ~/bin/wpm/wpmsource" >> ~/.bashrc
+
+source ~/.bashrc
+
